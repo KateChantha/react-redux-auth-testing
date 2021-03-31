b626d57bcbf4ee27242f33fbf19547985d84c2bf
Kato <kubkate@gmail.com>
2021-03-30 19:29:00 -0700

    implement fecth data from jsonplaceholde api

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise'
 import reducers from 'reducers'
 
 /**
@@ -12,8 +13,14 @@ import reducers from 'reducers'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ children, initialState = {}}) => {
+  const store = createStore(
+    reducers, 
+    initialState, 
+    applyMiddleware(reduxPromise)
+  )
+
   return (
-    <Provider store={createStore(reducers, initialState)} >
+    <Provider store={store} >
       { children }
     </Provider>
   )
